d2c78e6d08b167c3a274cc9b9b7352381ff4ea25
bytedefined <56295147+Bytedefined@users.noreply.github.com>
2022-09-07 21:34:29 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@
       <p class="text-gray-300 mt-1 max-w-lg font-medium text-center">
         A Russian/Australian software developer/designer.
       </p>
-      <div class="flex gap-2 mx-auto mt-2 flex-wrap justify-center items-center">
+      <div class="flex gap-2 mx-auto mt-2 flex-wrap justify-center items-center w-full">
         <a href="https://twitter.com/Bytedefined" target="_blank">
           <InteractiveButton
             class="duration-500 shadow-none !bg-blue-600 hover:!bg-blue-500 whitespace-nowrap w-full"
