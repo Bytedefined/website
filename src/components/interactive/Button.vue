f9557e8ac94095748eddd5c36be0d139ce32dacf
bytedefined <56295147+Bytedefined@users.noreply.github.com>
2022-09-07 21:04:34 +0800

    Update Button.vue

--- src/components/interactive/Button.vue
+++ src/components/interactive/Button.vue
@@ -7,7 +7,7 @@
   </button>
   <button
     type="button"
-    class="bg-zinc-700/50 select-none hover:bg-polar-500 shadow-md duration-300 px-4 py-6 h-8 rounded-lg text-sm font-medium tracking-wide text-whites-200 whitespace-normal hidden-desktop"
+    class="bg-zinc-700/50 select-none hover:bg-polar-500 shadow-md duration-300 px-4 py-6 h-8 rounded-lg text-sm font-medium tracking-wide text-whites-200 whitespace-nowrap hidden-desktop"
   >
     <slot>{{ text }}</slot>
   </button>
