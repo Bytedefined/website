7219d9804ee2d5f6e96df7f1ddbf516dff54d5b3
bytedefined <56295147+Bytedefined@users.noreply.github.com>
2022-09-10 15:21:30 +0800

    General improvements

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,10 @@
 <template>
-  <footer class="flex justify-center gap-2 pb-3">
+  <footer class="text-center gap-2 pb-3">
     <p
         class="text-neutral-300 text-sm"
     >
       Copyright © {{new Date().getFullYear()}} <strong>Bytedefined</strong>. All rights reserved.
     </p>
-  </footer>
-  <footer class="flex justify-center gap-2 pb-3">
     <p class="text-neutral-300 text-sm">
       Built with <a href="https://tailwindcss.com" target="_blank">tailwindcss</a> and <a href="https://nuxtjs.org" target="_blank">NuxtJS</a>. Background by <a href="https://jet.pics" target="_blank">Jay</a>.
     </p>
