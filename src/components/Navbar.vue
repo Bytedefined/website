360517b18420a9d0a6f5b9d7fa43496c6efa2a70
bytedefined <56295147+Bytedefined@users.noreply.github.com>
2022-09-10 22:36:30 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,7 +17,7 @@
     <aside class="pt-4 px-4 transform top-0 left-0 w-64 bg-sidebar fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30" :class="sidebar ? 'translate-x-0' : '-translate-x-full'">
       <h1 class="text-2xl text-blues-200 tracking-wider font-semibold mb-2 text-center">Bytedefined</h1>
       <ul>
-        <NuxtLink v-for="item in items" :key="item.id" :to="item.route" v-on:click="sidebar = true" class="override">
+        <NuxtLink v-for="item in items" :key="item.id" :to="item.route" v-on:click="sidebar = false" class="override">
           <li :class="$route.path == item.route ? 'bg-sidebarItem' : null" class="pl-4 w-54 mb-2 py-2 text-white rounded h-40px cursor-pointer hover:bg-sidebarItem">
               <i :class="item.icon"></i>&nbsp;&nbsp;{{item.name}}
           </li>
